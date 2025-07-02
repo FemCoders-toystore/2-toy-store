361b0d8ccdfe9a5ab3ead2123f9299e977d314d7
Alexandracoder <acasaviajes@gmail.com>
2025-07-02 14:22:12 +0200

    Initial commit from Spring Initializr

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,54 @@
+package model;
+
+import jakarta.validation.constraints.NotBlank;
+
+    @Entity
+    @Table(name = "products")
+    public class Product {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long Id;
+
+        @NotBlank(message = "Product name is required")
+        private String name;
+
+        private double price;
+        private String imageUrl;
+        private boolean featured;
+
+        @ManyToOne
+        @JoinColumn(name = "category_id")
+        private Category category;
+
+        public Product() {
+        }
+
+        public Product(String name, Double price, String imageUrl, Boolean featured) {
+            this.name = name;
+            this.price = price;
+            this.imageUrl = imageUrl;
+            this.featured = featured;
+        }
+
+        public Long getId() {
+            return Id;
+        }
+
+        public void setId(Long id) {
+            Id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+    }
---
+++ src/test/java/com/foreign/team/toy/store/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foreign.team.toy.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
